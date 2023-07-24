8cd6e31ac2b02dbbfaa0c8aa7b29553cb7d24dd9
george.smith <litesoft@gmail.com>
2023-07-24 13:47:27 -0700

    New V2 form

---
+++ v2/calculator.go
@@ -0,0 +1,29 @@
+package calculator
+
+// Add two integers
+//
+//goland:noinspection ALL
+func Add(a int, b int) int {
+	return a + b
+}
+
+// Subtract two integers
+//
+//goland:noinspection ALL
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+// Multiply two integers
+//
+//goland:noinspection ALL
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+// Divide two integers
+//
+//goland:noinspection ALL
+func Divide(a int, b int) int {
+	return a / b
+}
